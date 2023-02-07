870074446d3a057f78d6f2b203e277c6257f498c
Julian Gindi <julian@gindi.io>
2023-02-07 14:54:43 -0800

    added code to show textual number of seconds remaining

--- OpenTimer/TimerView.swift
+++ OpenTimer/TimerView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct TimerView: View {
-    @State var timerAmount: Float = 60.0 // In seconds
+    let timerAmount: Float
+    @State var timeRemaining: Float = 0.0
     @State var startTimer = false
     @State private var completionAmount = 1.0
-    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    // Timers
+    let animationTimer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    let mainTimer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
     
     var tickCount: Double {
@@ -20,28 +24,46 @@ struct TimerView: View {
     }
     
     var body: some View {
+        
         VStack {
-            Text("\(Int(timerAmount)) seconds")
-                .font(.title)
-                .multilineTextAlignment(.center)
-            Circle()
-                .trim(from: 0.0, to: completionAmount)
-                .stroke(.blue, lineWidth: 20)
-                .frame(width: 200, height: 200)
-                .rotationEffect(.degrees(-90))
-                .onReceive(timer) { _ in
-                    if startTimer {
-                        withAnimation {
-                            if completionAmount == 0.0 {
-                                // Play a notification sound and display some text
-                            } else {
-                                completionAmount -= tickCount
+            ZStack {
+                if startTimer {
+                    Text("\(Int(timeRemaining)) seconds")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                } else {
+                    Text("\(Int(timerAmount)) seconds")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                }
+                Circle()
+                    .trim(from: 0.0, to: completionAmount)
+                    .stroke(.blue, lineWidth: 20)
+                    .frame(width: 200, height: 200)
+                    .rotationEffect(.degrees(-90))
+                    .onReceive(animationTimer) { _ in
+                        // Hook for animating the circle via the `animationTimer`
+                        if startTimer {
+                            withAnimation {
+                                if completionAmount == 0.0 {
+                                    // Play a notification sound and display some text
+                                } else {
+                                    completionAmount -= tickCount
+                                }
                             }
                         }
                     }
-                }
+                    .onReceive(mainTimer) { _ in
+                        if startTimer {
+                            if timeRemaining > 0.0 {
+                                timeRemaining -= 1.0
+                            }
+                        }
+                    }
+            }
             Button {
                 startTimer = true
+                timeRemaining = timerAmount
             } label: {
                 Text("Start")
                     .padding(20)
@@ -53,6 +75,6 @@ struct TimerView: View {
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerView()
+        TimerView(timerAmount: 60.0)
     }
 }
